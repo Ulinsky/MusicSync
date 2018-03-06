fe2fb4363b72861c595195c0ca4e3028edea34ca
Ulinsky <ulinsky123@hotmail.com>
2018-03-06 15:26:07 +0100

    Initial version

---
+++ src/MainPanel.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainPanel extends JPanel implements ActionListener {
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 600;
+    private JFileChooser chooser;
+    private String desktopPath, androidPath;
+    private JButton setDesktopPath, setAndroidPath, syncSongs;
+    private JLabel desktopPathLabel, androidPathLabel;
+
+    public MainPanel() {
+        super(new BorderLayout());
+        setSize(new Dimension(HEIGHT, WIDTH));
+        JPanel upperPanel = new JPanel();
+        setDesktopPath = new JButton("Select desktop path");
+        upperPanel.add(setDesktopPath, BorderLayout.NORTH);
+        setAndroidPath = new JButton("Select android path");
+        upperPanel.add(setAndroidPath, BorderLayout.SOUTH);
+        JPanel middlePanel = new JPanel();
+        desktopPathLabel = new JLabel("No desktop path set");
+        androidPathLabel = new JLabel("No android path set");
+        middlePanel.add(desktopPathLabel, BorderLayout.WEST);
+        middlePanel.add(androidPathLabel, BorderLayout.EAST);
+        setDesktopPath.addActionListener(this);
+        setAndroidPath.addActionListener(this);
+        add(upperPanel, BorderLayout.NORTH);
+        add(middlePanel, BorderLayout.CENTER);
+        JPanel lowerPanel = new JPanel();
+        syncSongs = new JButton("Sync songs");
+        syncSongs.addActionListener(this);
+        lowerPanel.add(syncSongs);
+        add(lowerPanel, BorderLayout.SOUTH);
+    }
+
+    public static void showGui() {
+        JFrame frame = new JFrame("MusicSync");
+        frame.setSize(new Dimension(HEIGHT, WIDTH));
+        frame.add(new MainPanel());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == setDesktopPath || e.getSource() == setAndroidPath) {
+            chooser = new JFileChooser();
+            chooser.setAcceptAllFileFilterUsed(false);
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.setApproveButtonText("Select");
+            int selection = chooser.showOpenDialog(null);
+            if (selection == JFileChooser.APPROVE_OPTION && e.getSource() == setDesktopPath) {
+                desktopPath = chooser.getSelectedFile().getPath();
+                desktopPathLabel.setText("Desktop path set");
+                System.out.println(desktopPath);
+            } else if (selection == JFileChooser.APPROVE_OPTION && e.getSource() == setAndroidPath) {
+                androidPath = chooser.getSelectedFile().getPath();
+                androidPathLabel.setText("Android path set");
+                System.out.println(androidPath);
+            }
+        } else if (e.getSource() == syncSongs) {
+            if (desktopPath == null || androidPath == null) {
+                new JOptionPane().createDialog("Either desktop or android path was not selected");
+            }
+
+        }
+    }
+    public List<File> listFilesForFolder(final File folder) {
+        List<File> songs = new ArrayList<>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+               songs.add(fileEntry);
+            }
+        }
+        return songs;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,20 @@
+import sun.applet.Main;
+
+import javax.swing.*;
+import java.io.File;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
-    }
+       /* JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+       File[] files =  fileChooser.getSelectedFiles();
+
+       for(File f : files){
+           System.out.println(f.getName());
+       }*/
+
+           MainPanel.showGui();
+
+       }
+
 }
