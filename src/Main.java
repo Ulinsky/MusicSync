d484607975a94196dd95805a0918d3db891e9f3b
Ulinsky <ulinsky123@hotmail.com>
2018-03-11 22:24:01 +0100

    streamlined usage of jMPTe test method

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 
-import be.derycke.pieter.com.COMException;
-import jmtp.*;
+import jmtp.PortableDevice;
+import jmtp.PortableDeviceManager;
+import jmtp.PortableDeviceObject;
+import jmtp.PortableDeviceStorageObject;
 import musicsync.MusicSyncView;
-
-import java.io.File;
-import java.math.BigInteger;
 
 public class Main {
 
         public static void main(String[] args) throws Throwable {
-            jMTPeMethodTest();
+            MusicSyncView.drawGUI();
+          // jMTPeMethodTest();
         }
 
     private static void jMTPeMethodTest()
     {
-        PortableDeviceFolderObject targetFolder = null;
         PortableDeviceManager manager = new PortableDeviceManager();
+        if(manager.getDevices().length==0){
+            System.out.println("No device connected");
+            return;
+        }
         PortableDevice device = manager.getDevices()[0];
         // Connect to USB tablet
         device.open();
         System.out.println(device.getModel());
-
         System.out.println("---------------");
-
         // Iterate over deviceObjects
         for (PortableDeviceObject object : device.getRootObjects())
         {
@@ -30,49 +31,14 @@ public class Main {
             if (object instanceof PortableDeviceStorageObject)
             {
                 PortableDeviceStorageObject storage = (PortableDeviceStorageObject) object;
-
                 for (PortableDeviceObject o2 : storage.getChildObjects())
                 {
-                    if(o2.getOriginalFileName().equalsIgnoreCase("testFolder"))
-                    {
-                        targetFolder = (PortableDeviceFolderObject) o2;
-                    }
-
                     System.out.println(o2.getOriginalFileName());
                 }
-
-                copyFileFromComputerToDeviceFolder(targetFolder);
-                PortableDeviceObject[] folderFiles = targetFolder.getChildObjects();
-                for (PortableDeviceObject pDO : folderFiles) {
-                    copyFileFromDeviceToComputerFolder(pDO, device);
-                }
-
             }
         }
-
         manager.getDevices()[0].close();
     }
 
-    private static void copyFileFromDeviceToComputerFolder(PortableDeviceObject pDO, PortableDevice device)
-    {
-        PortableDeviceToHostImpl32 copy = new PortableDeviceToHostImpl32();
-        try {
-            copy.copyFromPortableDeviceToHost(pDO.getID(), "C:\\TransferTest", device);
-        } catch (COMException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-    private static void copyFileFromComputerToDeviceFolder(PortableDeviceFolderObject targetFolder)
-    {
-        BigInteger bigInteger1 = new BigInteger("123456789");
-        File file = new File("C:\\GettingJMTP.pdf");
-        try {
-            targetFolder.addAudioObject(file, "jj", "jj", bigInteger1);
-        } catch (Exception e) {
-            System.out.println("Exception e = " + e);
-        }
-    }
 }
 
