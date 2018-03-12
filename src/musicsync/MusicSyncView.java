042f6063b88bb9c532e409d2407a29799b060676
Ulinsky <ulinsky123@hotmail.com>
2018-03-12 02:15:49 +0100

    code clean up, TODO separate model and GUI

--- src/musicsync/MusicSyncView.java
+++ src/musicsync/MusicSyncView.java
@@ -14,21 +14,21 @@ import static javax.swing.JOptionPane.showMessageDialog;
 
 public class MusicSyncView extends JPanel {
     private static final int HEIGHT = 100;
-    private static final int WIDTH = 800;
+    private static final int WIDTH = 700;
 
     private MusicSyncView() {
         super(new BorderLayout());
         super.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        JTextField firstPathText = new JTextField("First folder", 20);
-        JTextField secondPathText = new JTextField("Second folder", 30);
-        firstPathText.setEditable(false);
-        secondPathText.setEditable(false);
+        JTextField desktopPath = new JTextField("Desktop folder", 25);
+        JTextField androidPath = new JTextField("Android music folder", 18);
+        desktopPath.setEditable(false);
+        androidPath.setEditable(false);
 
         JPanel upperHalf = new JPanel(new BorderLayout());
         JPanel lowerHalf = new JPanel();
-        upperHalf.setBorder(BorderFactory.createEmptyBorder(10, 100, 0, 100));
-        upperHalf.add(firstPathText, BorderLayout.LINE_START);
-        upperHalf.add(secondPathText, BorderLayout.LINE_END);
+        upperHalf.setBorder(BorderFactory.createEmptyBorder(10, 50, 0, 50));
+        upperHalf.add(desktopPath, BorderLayout.LINE_START);
+        upperHalf.add(androidPath, BorderLayout.LINE_END);
 
         JButton setDesktopPathButton = new JButton("Select desktop path");
         JButton syncSongsButton = new JButton("First choose a folder");
@@ -36,7 +36,7 @@ public class MusicSyncView extends JPanel {
         syncSongsButton.setBackground(Color.RED);
 
         lowerHalf.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
-        lowerHalf.add(setDesktopPathButton,BorderLayout.LINE_START);
+        lowerHalf.add(setDesktopPathButton, BorderLayout.LINE_START);
         lowerHalf.add(syncSongsButton, BorderLayout.LINE_END);
         this.add(upperHalf, BorderLayout.NORTH);
         this.add(lowerHalf, BorderLayout.SOUTH);
@@ -44,13 +44,12 @@ public class MusicSyncView extends JPanel {
         setDesktopPathButton.addActionListener(actionEvent -> {
             JFileChooser chooser = new JFileChooser();
             chooser.setAcceptAllFileFilterUsed(false);
-            //choose only folders
-            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); //choose only folders
             chooser.setApproveButtonText("Select");
             int selection = chooser.showOpenDialog(null);
             if (selection == JFileChooser.APPROVE_OPTION) {
-                firstPathText.setText(chooser.getSelectedFile().getPath());
-                secondPathText.setText(findAndroidPath());
+                desktopPath.setText(chooser.getSelectedFile().getPath());
+                androidPath.setText(findAndroidPath());
                 syncSongsButton.setBackground(Color.WHITE);
                 syncSongsButton.setText("Sync songs");
                 syncSongsButton.setEnabled(true);
@@ -58,7 +57,8 @@ public class MusicSyncView extends JPanel {
         });
 
         syncSongsButton.addActionListener((ActionEvent actionEvent) -> {
-            showMessageDialog(null, "Not implemented yet");
+            //showMessageDialog(null, "Not implemented yet");
+            List<File> desktopSongs = listFilesForFolder(new File(desktopPath.getText()));
         });
     }
 
@@ -72,7 +72,6 @@ public class MusicSyncView extends JPanel {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
 
     @SuppressWarnings("ConstantConditions")
     private List<File> listFilesForFolder(final File folder) {
@@ -91,44 +90,7 @@ public class MusicSyncView extends JPanel {
         return songs;
     }
 
-    private void copyMissing(List<File> source, List<File> destination, String secondPath) {
-        if (source == null || destination == null || secondPath == null) {
-            showMessageDialog(null, "Error.");
-            return;
-        }
-        boolean found;
-        if (source.isEmpty()) {
-            showMessageDialog(null, "Source path contains no songs.");
-            return;
-        }
-        int counter = 0;
-        for (File toCompare : source) { //iterates first list
-            found = false; //assume the song is not present
-            for (File compareTo : destination) { //iterate second list
-                if (toCompare.getName().equals(compareTo.getName())) { //song found
-                    found = true;
-                }
-            }
-            if (!found) { //case song not find, copy it to second directory
-                try {
-                    InputStream in = new FileInputStream(toCompare.getPath()); //file to be copied
-                    String destPath = secondPath.concat("\\" + toCompare.getName()); //destination where to copy
-                    OutputStream out = new FileOutputStream(destPath);
-                    byte[] buf = new byte[(int) toCompare.length()]; //buffer
-                    int len;
-                    while ((len = in.read(buf)) > 0) {
-                        out.write(buf, 0, len); //write process
-                    }
-                    in.close(); //done writing
-                    out.close();
-                    counter++;
-                } catch (Exception e) {
-                    showMessageDialog(null, "Error while writing song.");
-                }
-            }
-        }
-        showMessageDialog(null, counter == 1 ? counter + " song has been copied." : counter + " songs have been copied.");
-    }
+
 
     private String findAndroidPath() {
         PortableDeviceManager manager = new PortableDeviceManager();
@@ -149,7 +111,6 @@ public class MusicSyncView extends JPanel {
         manager.getDevices()[0].close();
         return device.getFriendlyName() + "\\" + musicFolder.getParent().getName() + "\\" + musicFolder.getName(); //TODO make it NOT hardcoded
     }
-
 
     private PortableDeviceFolderObject findMusicFolder(PortableDevice device) {
         //Iterate through root folders
